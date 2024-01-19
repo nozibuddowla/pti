1a81b2ecad0e7e1529049de26b170df82b88ec8a
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2024-01-19 07:25:56 +0600

    Navbar added

---
+++ src/Representational/Header.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { BiUser } from "react-icons/bi";
+
+const Header = () => {
+    const navmenu = (
+        <>
+          <li>
+            <details>
+              <summary className="bg-white font-medium uppercase">Menu</summary>
+              <ul className="p-2 w-40 normal-case md:static md:right-0
+              absolute right-8 bg-[#EEEFF0]">
+                <li className="hover:text-[#F99F1C]">
+                  <a href="/">Home</a>
+                </li>
+                <li className="hover:text-[#F99F1C]">
+                  <a href="/details">Details</a>
+                </li>
+                <li className="hover:text-[#F99F1C]">
+                  <a href="/category">Category</a>
+                </li>
+                <li className="hover:text-[#F99F1C]">
+                  <a href="/favorite-food">My Favorite Food</a>
+                </li>
+                <li className="hover:text-[#F99F1C]">
+                  <a href="/profile">Profile</a>
+                </li>
+                <li className="hover:text-[#F99F1C]">
+                  <a href="/login-signup">Login/Sign Up</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </>
+      );
+      return (
+        <div>
+          <div className="navbar">
+            <div className="md:navbar-start">
+              <a href='/' className="btn text-black btn-ghost text-2xl font-bold">pti.</a>
+            </div>
+            <div className="md:navbar-center relative navbar-start md:w-2/5 w-full bg-[#EEEFF0]">
+              <div className="px-3 absolute left-0">
+                <CiSearch className="text-lg bg-[#EEEFF0]" />
+              </div>
+              <input type="text" placeholder="Find Your Food" className="form-control input pl-9 md:text-sm text-xs input-bordered input-sm focus:outline-none md:w-full w-3/4 max-w-md bg-[#EEEFF0]" />
+              <ul className="menu text-sm menu-horizontal px-1">{navmenu}</ul>
+            </div>
+            <div className="navbar-end hidden lg:flex ">
+                <button className='btn bg-[#F99F1C] rounded-full text-2xl w-12 font-bold'>
+                  <BiUser />
+                </button>
+            </div>
+        </div>
+      </div>
+      )
+}
+export default Header
---
+++ src/component/MainComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Representational/Header";
+
+const MainComponent = () => {
+  return (
+    <div className="bg-[#EEEFF0]">
+      <div className="max-w-screen-xl mx-auto md:px-4">
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default MainComponent;
